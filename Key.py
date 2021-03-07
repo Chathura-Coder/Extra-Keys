19aad030991ad12ab9cc5fc7d9caa96e9c12b4fa
Chathura-Coder <78437331+Chathura-Coder@users.noreply.github.com>
2021-03-07 18:24:17 +0530

    Add files via upload

---
+++ Key.py
@@ -0,0 +1,39 @@
+import os
+from time import sleep
+
+#by Zheck Savalas
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+os.system('clear')
+print(a+'\t  Shorcut for help you')
+print(b+'\t    CHATHURA JANITH')
+print('\t https://github.com/Chathura-Coder')
+print(a+'+'*40)
+print('\nProses..')
+sleep(1)
+print(b+'\n[!] making termux properties directory..')
+sleep(1)
+try:
+      os.mkdir('/data/data/com.termux/files/home/.termux')
+except:
+      pass
+print(a+'[!]Success !')
+sleep(1)
+print(b+'\n[!] Making setup file..')
+sleep(1)
+
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP','ENTER'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN','CLEAR']]"
+kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
+kontol.write(key)
+kontol.close()
+sleep(1)
+print(a+'[!] Success !')
+sleep(1)
+print(b+'\n[!] Setting up..')
+sleep(2)
+os.system('termux-reload-settings')
+
+# ini cuma shortcut buat bantu para nub
+# Zheck Savalas
